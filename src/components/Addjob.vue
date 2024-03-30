f295a2e2ffd22b0b2f2f360c34f2202a16d640f7
slit09 <maryva@mail.ru>
2024-03-30 16:45:30 +0300

    123

--- src/components/Addjob.vue
+++ src/components/Addjob.vue
@@ -1,23 +1,16 @@
 <template>
   <div class="rows">
-  <h1> Принятие на работу </h1>
+  <h1> Предложить для картины идеи </h1>
       <div>
           <div class="mb-3">
-          <label for="CatName" class="form-label">Кличка</label>
+          <label for="CatName" class="form-label">тема</label>
           <input v-model="staff.nickname" type="text" class="form-control" id="CatName" aria-describedby="emailHelp">
       </div>
       <div class="mb-3">
-        <label for="catAge" class="form-label">Возраст</label>
+        <label for="catAge" class="form-label">дополнительное описание</label>
         <input v-model="staff.age" type="text" class="form-control" id="catAge">
       </div>
-      <div class="mb-3">
-        <label for="catJobDate" class="form-label">Дата приема на работу</label>
-        <input v-model = "staff.dateStartJob" type="date" class="form-control" id="catJobDate">
-      </div>
-      <div class="mb-3">
-        <label for="catStreet" class="form-label">Место патрулирования</label>
-        <input v-model = "staff.street" type="date" class="form-control" id="catJobDate">
-      </div>
+
       <button v-on:click="addJob" class="btn btn-primary">Сохранить</button>
       </div>
   </div>
